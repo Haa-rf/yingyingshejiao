5630b68f4e08d3c1360d8e2ee6b207261a63a9ef
ScoutB <37256184+ScoutB@users.noreply.github.com>
2018-03-13 02:38:16 +0000

    Create User.java

---
+++ app/src/main/java/com/example/User/User.java
@@ -0,0 +1,26 @@
+package com.example.User;
+
+/**
+ * Created by ScoutB on 2018/3/12.
+ */
+
+public class User {
+    private String username;
+    private String pwd;
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPwd(){
+        return pwd;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPwd(String pwd){
+        this.pwd=pwd;
+    }
+}

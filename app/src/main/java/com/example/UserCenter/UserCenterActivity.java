967239548d7f423825de6f90942810f705d06774
Renfu Go <1779356953@qq.com>
2018-03-14 16:21:25 +0800

    modify

---
+++ app/src/main/java/com/example/UserCenter/UserCenterActivity.java
@@ -0,0 +1,15 @@
+package com.example.UserCenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.R;
+
+public class UserCenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_center);
+    }
+}
